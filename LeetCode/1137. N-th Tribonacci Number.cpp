a00b2b8d773834d9cee6d9c5770f58b372b3d7e7
Harsh <128832204+4444Harsh@users.noreply.github.com>
2025-08-02 11:39:10 +0530

    solve 1137. N-th Tribonacci Number.cpp

---
+++ LeetCode/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n < 3)
+            return 1;
+
+        int first = 0, second = 1, third = 1, cur;
+
+        for (int i = 3; i <= n; i++) {
+            cur = first + second + third;
+            first = second;
+            second = third;
+            third = cur;
+        }
+
+        return third;
+    }
+};
